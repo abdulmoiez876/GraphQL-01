7af3184bfd34c6e7485c1c3174e628266083f787
abdulmoiez876 <abdul.moiez876@gmail.com>
2023-05-27 12:09:07 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLNonNull, GraphQLList } from 'graphql';
+
+const app = express();
+
+const authors = [
+	{ id: 1, name: 'J. K. Rowling' },
+	{ id: 2, name: 'J. R. R. Tolkien' },
+	{ id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+	{ id: 5, name: 'The Two Towers', authorId: 2 },
+	{ id: 6, name: 'The Return of the King', authorId: 2 },
+	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
+	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+const bookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLInt) }
+    })
+})
+
+const rootQueryType = new GraphQLObjectType({
+    name: 'rootQuery',
+    fields: () => ({
+        books: {
+            type: GraphQLList(bookType),
+            resolve: () => books
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: rootQueryType
+})
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema
+}))
+
+app.listen(8000, () => {
+    console.log('Server started!');
+})
